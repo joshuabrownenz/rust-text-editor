0dff74381c63fda15f31558814a2b06962b17fd3
Joshua Browne <joshuabrownenz@gmail.com>
2024-03-31 17:47:22 +1300

    Feat: Cleanup and not Canonical mode

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,35 @@ use termios::*;
 mod error;
 pub mod prelude;
 
+struct Cleanup {
+    original_termios: Option<Termios>,
+}
+
+impl Cleanup {
+    pub fn default() -> Cleanup {
+        Cleanup {
+            original_termios: None
+        }
+    }
+
+    pub fn set_original_termios(&mut self, original_terminal: Termios) {
+        self.original_termios = Some(original_terminal);
+    }
+}
+
+impl Drop for Cleanup {
+    fn drop(&mut self) {
+        println!("Running cleanup");
+        if let Some(original_termios) = &self.original_termios {
+            disable_terminal(original_termios);
+        }
+    }
+}
+
 fn main() {
-    enable_raw_mode();
+    let mut cleanup = Cleanup::default();
+
+    enable_raw_mode(&mut cleanup);
 
     let mut buf: [u8; 1] = [0; 1];
     loop {
@@ -28,12 +55,20 @@ fn main() {
     }
 }
 
-fn enable_raw_mode() {
-    let mut termios = Termios::from_fd(io::stdin().as_raw_fd()).unwrap();
+fn enable_raw_mode(cleanup: &mut Cleanup) {
+    let original_termios = Termios::from_fd(io::stdin().as_raw_fd()).unwrap();
+
+    let mut termios = original_termios;
 
     // Turn off ECHO
-    termios.c_lflag &= !(ECHO);
+    termios.c_lflag &= !(ECHO | ICANON);
 
     // Apply the updated termios settings
     tcsetattr(io::stdin().as_raw_fd(), TCSANOW, &termios).unwrap();
+
+    cleanup.set_original_termios(original_termios);
+}
+
+fn disable_terminal(original_terminal: &Termios) {
+    tcsetattr(io::stdin().as_raw_fd(), TCSANOW, original_terminal).unwrap();
 }
